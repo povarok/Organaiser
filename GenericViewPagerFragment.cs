6b77bad0ddc3a8e156f4911892ff66c96a6bbacd
Vasily <vasily.komarov1997@gmail.com>
2017-10-17 23:46:46 +0500

    Revert "added tabs"
    This reverts commit 136dda5e91481ac4373443053f35f574e98c042e.

--- GenericViewPagerFragment.cs
+++ GenericViewPagerFragment.cs
@@ -12,18 +12,7 @@ using Android.Widget;
 
 namespace AndroidSQLite
 {
-    public class GenericViewPagerFragment : Android.Support.V4.App.Fragment
+    class GenericViewPagerFragment 
     {
-        private Func<LayoutInflater, ViewGroup, Bundle, View> _view;
-        public GenericViewPagerFragment(Func<LayoutInflater, ViewGroup, Bundle, View> view)
-        {
-            _view = view;
-        }
-
-        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
-        {
-            base.OnCreateView(inflater, container, savedInstanceState);
-            return _view(inflater, container, savedInstanceState);
-        }
     }
 }
